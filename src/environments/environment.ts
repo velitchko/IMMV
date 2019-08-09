da23669f35008c9242b50021c5f16ec6d33975d2
Velitchko Filipov <velitchkofilipov@gmail.com>
2019-08-09 13:59:15 +0200

    added app url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,8 @@ export const environment = {
   MAPBOX_API_KEY: 'pk.eyJ1IjoidmVsaXRjaGtvIiwiYSI6ImNqOGJkeXE1dzBobWYzMnFybW4zeHJ6eWwifQ.kqTgrJJpLhuqLpm-LL5ghQ',
   GMAPS_API_KEY: 'AIzaSyD0b5xJBAt5-SchLgwQ83YHzLo51ZfEvCA',
   GPLACES_API_KEY: 'AIzaSyAZJmjLUv7d5SB1XbL96t-7cNd2eZKBd5Y',
-  API_URL: 'http://localhost:4000/api/v1/'
+  API_URL: 'http://localhost:4000/api/v1/',
+  APP_URL: 'http://localhost:4000/'
 };
 
 /*
