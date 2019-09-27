0b91a1e38032eed75ca930206eba53d16fb729bb
Velitchko Filipov <velitchkofilipov@gmail.com>
2019-09-27 13:20:21 +0200

    renaming component added preset route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,14 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent }   from './components/dashboard/dashboard.component';
 import { ThemeComponent }       from './components/themes/themes.component';
 import { MusicMapComponent }    from './components/music-map/music-map.component';
-import { PersonOrganizationComponent } from './components/personorganization/personorganization.component';
+import { NetworkComponent } from './components/network/network.component';
 import { PeopleTimelineComponent } from './components/people-timeline/people-timeline.component';
 import { BiographicalComponent } from './components/biographical.component/biographical.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'music-map', component: MusicMapComponent },
-  { path: 'people', component: PersonOrganizationComponent },
+  { path: 'network', component: NetworkComponent },
+  { path: 'network/:preset', component: NetworkComponent },
   { path: 'themes', component: ThemeComponent },
   { path: 'biographical/:preset', component: BiographicalComponent },
   { path: 'biographical', component: BiographicalComponent },
