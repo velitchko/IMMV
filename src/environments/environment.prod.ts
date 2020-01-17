1778b7318fd4bd3645bd2fcc0253feb55370dad7
Velitchko Filipov <velitchkofilipov@gmail.com>
2020-01-17 14:45:20 +0100

    Updating whole repo

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  MAPBOX_API_KEY: 'pk.eyJ1IjoidmVsaXRjaGtvIiwiYSI6ImNqOGJkeXE1dzBobWYzMnFybW4zeHJ6eWwifQ.kqTgrJJpLhuqLpm-LL5ghQ',
-  GMAPS_API_KEY: 'AIzaSyD0b5xJBAt5-SchLgwQ83YHzLo51ZfEvCA',
-  GPLACES_API_KEY: 'AIzaSyAZJmjLUv7d5SB1XbL96t-7cNd2eZKBd5Y',
-  API_URL: 'https://immv-app.cvast.tuwien.ac.at/api/v1/',
-  APP_URL: 'https://immv-app.cvast.tuwien.ac.at/'
-};
