c545b2d250fca4999fd299af06a3f39967e0a891
Velitchko Filipov <velitchkofilipov@gmail.com>
2020-01-31 12:04:25 +0100

    added: info component

---
+++ src/app/components/info.component/info.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+    selector: 'app-info',
+    templateUrl: './info.component.html',
+    styleUrls: ['./info.component.scss']
+})
+
+export class InfoComponent {
+
+    constructor() {}
+}
