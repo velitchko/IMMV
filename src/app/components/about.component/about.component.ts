5b69817c00958088f2764de50b8ec75f342b8aa9
Velitchko Filipov <velitchkofilipov@gmail.com>
2020-01-31 12:04:14 +0100

    added: about component

---
+++ src/app/components/about.component/about.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
+})
+
+export class AboutComponent {
+
+    constructor() {}
+}
