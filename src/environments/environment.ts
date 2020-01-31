6e713bb16b69788f75709c5e1aa401b9817ad661
Velitchko Filipov <velitchkofilipov@gmail.com>
2020-01-31 12:04:41 +0100

    updated: port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment = {
   MAPBOX_API_KEY: 'pk.eyJ1IjoidmVsaXRjaGtvIiwiYSI6ImNqOGJkeXE1dzBobWYzMnFybW4zeHJ6eWwifQ.kqTgrJJpLhuqLpm-LL5ghQ',
   GMAPS_API_KEY: 'AIzaSyD0b5xJBAt5-SchLgwQ83YHzLo51ZfEvCA',
   GPLACES_API_KEY: 'AIzaSyAZJmjLUv7d5SB1XbL96t-7cNd2eZKBd5Y',
-  API_URL: 'http://localhost:4200/api/v1/',
-  APP_URL: 'http://localhost:4200/'
+  API_URL: 'http://localhost:4000/api/v1/',
+  APP_URL: 'http://localhost:4000/'
 };
 
 /*
